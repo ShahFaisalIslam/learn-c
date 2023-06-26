965a03d6c90076fc7a5e1bc467d39ebf63b37bd9
ShahFaisalIslam <55596038+ShahFaisalIslam@users.noreply.github.com>
2023-06-27 01:34:54 +0500

    Create simplest_code_with_return.c
    This is the simplest code which is almost always the foundation for C codes. All the program does is provide 0 as its exit code. In Linux terminal, the variable $? will contain the exit code right after the program has been executed.

---
+++ simplest_code_with_return.c
@@ -0,0 +1,21 @@
+/*
+ * simplest_code_with_return.c
+ * This program does nothing except
+ * returning 0 to whatever program
+ * executed this code, which is most
+ * likely a Linux terminal.
+ * Zero here means "no errors"
+ * , and in more sophisticated
+ * programs, errors lead to non-zero
+ * return values
+ */
+
+/*
+ * int here means that the value
+ * returned by main() will be an
+ * integer, such as 0.
+*/
+int main()
+{
+  return 0;
+}
